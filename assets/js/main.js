ef28c744a14c7dd475f969a29200619ac604dd18
J.Alberto Azevedo <alazib7@gmail.com>
2023-06-15 19:54:34 +0200

    Header Navigaton WIP

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*==================== MENU SHOW AND HIDDEN ====================*/
+
+// const navMenu = document.getElementById("nav_menu")
+// const navToggle = document.getElementById("nav-toggle")
+// const navClose = document.getElementById("nav-close")
+
+// /*==================== MENU SHOW ====================*/
+// if (navToggle) {
+//   navToggle.addEventListener("click", () => {
+//     navMenu.classList.add("show-menu")
+//   })
+// }

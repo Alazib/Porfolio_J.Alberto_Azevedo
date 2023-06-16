4231d4b9a836bca277023950a9f6ebf5569b5f26
J.Alberto Azevedo <alazib7@gmail.com>
2023-06-16 17:47:35 +0200

    Home section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ if (navClose) {
   })
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*==================== REMOVE MENU MOBILE  ====================*/
 
 const navLink = document.querySelectorAll(".nav__link")
 
